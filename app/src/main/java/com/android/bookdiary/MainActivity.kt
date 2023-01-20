0e269fbf4d8debaf3318df4a097c0fcdf931eaee
thdek <thdekdms03@naver.com>
2023-01-20 15:45:22 +0900

    20230120 하단바 선택 시 해당 프래그먼트 이동, 달력 날짜와 선택시 이벤트까지 만들어놓음

--- app/src/main/java/com/android/bookdiary/MainActivity.kt
+++ app/src/main/java/com/android/bookdiary/MainActivity.kt
@@ -2,12 +2,12 @@ package com.android.bookdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     lateinit var mainFragment: MainFragment
-    lateinit var navigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +15,40 @@ class MainActivity : AppCompatActivity() {
 
         val mainFragment = MainFragment()
 
-        supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment).commit()
-        val navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation) //왜안돼!!!
+
+
+        supportFragmentManager.beginTransaction().add(R.id.container, mainFragment).commit()
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.tab1 -> {
+                val monthlyFragment = MonthlyFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, monthlyFragment)
+                    .commit()
+
+            }
+            R.id.tab2 -> {
+                val mainFragment = MainFragment()
+
+                supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment)
+                    .commit()
+
+            }
+            R.id.tab3 -> {
+                val dailyFragment = DailyFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, dailyFragment)
+                    .commit()
+
+            }
+
+
+        }
+
+        return true
+
     }
 }
+
