7119d286f975b6df206c3df35be61f8c1b7cc89b
hlhn5 <hlhn5745@naver.com>
2023-01-23 17:51:57 +0900

    20230123 17:51 DB

---
+++ app/src/main/java/com/android/bookdiary/DBManager.kt
@@ -0,0 +1,19 @@
+package com.android.bookdiary
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBManager( //4
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db!!.execSQL("CREATE TABLE bookDB (title text, author text, page INTEGER)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+}
