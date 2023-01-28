d914d55dc1ae220f02872eaa1d021aaa40ca956e
thdek <thdekdms03@naver.com>
2023-01-28 19:41:57 +0900

    20230128 CalendarAdapter, CalendarUtil, CalendarData, OnitemListener 주석까지 완료

--- app/src/main/java/com/android/bookdiary/CalendarData.kt
+++ app/src/main/java/com/android/bookdiary/CalendarData.kt
@@ -1,4 +1,4 @@
 package com.android.bookdiary
 
-data class CalendarData(val date : String, val color : String, val ratioPage : Int)
+data class CalendarData(val date : String, val color : String, val ratioPage : Int) //  CalendarAdapter에 전달할 데이터를 담기 위한 클래스
 
