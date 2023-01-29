325e9dbd5a91f8186093e11c3ae680072c910664
gayeoneez <gayeoneez@gmail.com>
2023-01-29 11:29:58 +0900

    20230129 11:29 이번달, 지난달 기록 없을 시 페퍼, 커버 보이게 수정

--- app/src/main/java/com/android/bookdiary/MainActivity.kt
+++ app/src/main/java/com/android/bookdiary/MainActivity.kt
@@ -12,44 +12,46 @@ import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-    class MainActivity : AppCompatActivity(),
-        BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
 
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            val mainFragment = MainFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment).commit()
-            val navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-            navigationView.setOnNavigationItemSelectedListener(this)
-        }
-
-        override fun onNavigationItemSelected(item: MenuItem): Boolean {
-            when (item.itemId) {
-
-                R.id.tab1 -> {
-                    val monthlyFragment = MonthlyFragment()
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, monthlyFragment)
-                        .commit()
-                }
-                R.id.tab2 -> {
-                    val mainFragment = MainFragment()
-
-                    supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment)
-                        .commit()
-                }
-                R.id.tab3 -> {
-                            val listFragment = ListFragment()
-                            supportFragmentManager.beginTransaction().replace(R.id.container, listFragment)
-                               .commit()
-                }
-            }
-            return true
-
-        }
+        val mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment).commit()
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigationView.setOnNavigationItemSelectedListener(this)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.tab1 -> {
+                val monthlyFragment = MonthlyFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, monthlyFragment)
+                    .commit()
+            }
+            R.id.tab2 -> {
+                val mainFragment = MainFragment()
+
+                supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment)
+                    .commit()
+            }
+            R.id.tab3 -> {
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, listFragment)
+                    .commit()
+            }
+        }
+        return true
+
+    }
+}
+
+
 
 
